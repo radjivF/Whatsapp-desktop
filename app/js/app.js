4888a4b821305b0b2dbf91236a2a56ee3c17e8b6
radjiv fender <carrereradjiv@yahoo.fr>
2015-04-15 11:13:05 +0200

    function windows close

--- app/js/app.js
+++ app/js/app.js
@@ -4,6 +4,16 @@ var nw = require('nw.gui');
 var win = nw.Window.get();
 var nativeMenuBar = new nw.Menu({ type: "menubar" });
 
+var platform = process.platform;
+platform = /^win/.test(platform) ? 'win32'
+         : /^darwin/.test(platform) ? 'osx64'
+         : 'linux' + (process.arch == 'ia32' ? '32' : '64');
+
+
+var isOSX = platform === 'osx64';
+var isWindows = platform === 'win32';
+var isLinux = platform.indexOf('linux') === 0;
+
 if (nativeMenuBar.createMacBuiltin) {
   nativeMenuBar.createMacBuiltin("whatsApp Desktop");
 }
@@ -31,4 +41,18 @@ window.onload = function() {
   })
 
 };
+
+// Don't quit the app when the window is closed
+win.on('close', function(quit) {
+  if (quit) {
+    win.close(true);
+  } else {
+    // On Linux, just minimize the window
+    if (isLinux) {
+      win.minimize();
+    } else {
+      win.hide();
+    }
+  }
+});
 
